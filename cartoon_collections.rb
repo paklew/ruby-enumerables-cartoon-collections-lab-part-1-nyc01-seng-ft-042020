9e9074f84c3d1c41d230f0d7188fbdfc2527267f
Pak Lew <paklew@yahoo.com>
2020-03-31 18:58:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |character|
-    p ""
+    p "Hello `#{character}`" + "!"
 end
 
 def list_dwarves(array)
